1c8d8c91289278b337e44152bad05f7ec05c0cb9
KumarGangesh123 <kumargangesh090@gmail.com>
2023-11-24 14:13:42 +0530

    Second Commit

--- src/Navbar/NavbarLinks.js
+++ src/Navbar/NavbarLinks.js
@@ -12,7 +12,7 @@ function NavbarLinks() {
     let value = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et aspernatur ducimus enim tenetur eligendi beatae necessitatibus quos animi sit, eos maxime porro. Animi rem earum doloribus necessitatibus incidunt, quibusdam expedita sunt aliquid, fuga voluptatum veniam asperiores! Temporibus quae ut architecto. Vero, quia repudiandae quos dignissimos ullam exercitationem totam iste, nemo, nulla natus dicta dolores laboriosam minus. Voluptatum tenetur nisi quod, eligendi recusandae ut at reprehenderit dolore consequatur, impedit, rem consequuntur perspiciatis iure id itaque enim. Unde beatae temporibus nulla dolores eveniet architecto voluptatibus dolorem vero officiis, aspernatur ad, dicta quibusdam cum et minima saepe est blanditiis. Sit, error aperiam. Libero quasi delectus aliquid officiis rem. Enim amet incidunt accusamus sapiente nesciunt quasi nam harum quo, dolores laborum impedit id eveniet corrupti quaerat fuga optio ut eum ab culpa sed odit facere possimus minus tempore? Qui, tempora, nam sunt itaque provident voluptate eum voluptates officiis ipsa deleniti fuga impedit non minima fugit sapiente et dolor praesentium, id labore autem culpa veritatis voluptatibus tenetur! Tenetur dicta cupiditate vel quos dignissimos provident obcaecati beatae blanditiis, suscipit corrupti adipisci temporibus sed, consequuntur culpa iure. Molestiae rem iusto similique doloremque et distinctio, fugiat eum mollitia voluptatem nisi nulla doloribus beatae laborum. Eveniet rerum molestias repudiandae?"
 
   return (
-    <Router>
+    <Router basename='/Navbar'>
         <div>
             <NavbarDesign />
             <Routes>
